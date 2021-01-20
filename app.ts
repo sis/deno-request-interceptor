cc6ddd986f69e65179a6482d6d1cd260f350f1ce
Simon Hoskins <simon.hoskins@imanage.com>
2021-01-20 12:36:05 +0000

    Improved

--- app.ts
+++ app.ts
@@ -5,37 +5,57 @@ const app = new Application();
 app.use(async (context) => {
   const { request: { method, url: { pathname } }} = context;
   const headers = await context.request.headers.entries();
+  const reqBody = await context.request.body({ type: 'raw' }).value;
 
+  const startDate = new Date();
   const start = performance.now();
   const url = `http://localhost:11040${pathname}`;
 
-  const res = await fetch(
-    url,
-    {
-      method,
-      headers: Array.from(headers),
-    }
-  );
+  try {
+    const res = await fetch(
+      url,
+      {
+        method,
+        headers: Array.from(headers),
+        body: reqBody,
+      }
+    );
+    
 
-  const end = performance.now();
-  const timeTaken = end - start;
-
-  console.log('time taken in ms:', timeTaken);
-
-  // Set the body
-  const responseText = await res.text();
-  context.response.body = responseText;
-
-  const resHeaderEntries = await res.headers.entries();
-  const resHeaders = Array.from(resHeaderEntries);
-
-  const text = await Deno.readTextFile("./data/logs.json");
-  let json = JSON.parse(text);
-  json.push({ timeTakenMs: timeTaken, url, responseText })
-  Deno.writeTextFile('./data/logs.json', JSON.stringify(json));
-  console.log('tejsonxt', json);
-
-  context.response.headers = new Headers(resHeaders);
+    const end = performance.now();
+    const timeTaken = end - start;
+  
+    console.log('time taken in ms:', timeTaken);
+  
+    // Set the body
+    const response = await res.text();
+    context.response.body = response;
+  
+    const resHeaderEntries = await res.headers.entries();
+    const resHeaders = Array.from(resHeaderEntries);
+  
+    const reqBodyText = await context.request.body({ type: 'text' }).value;
+    const date = new Date();
+    const json = {
+      timestamp: date.toISOString(),
+      requestStarted: startDate.toISOString(),
+      timeTakenMs: timeTaken,
+      url,
+      request: reqBodyText,
+      response,
+    };
+  
+    Deno.writeTextFile(
+      './data/logs.json',
+      `${JSON.stringify(json)}\n`,
+      { append: true, create: true },
+    );
+  
+    context.response.headers = new Headers(resHeaders);
+  } catch (e) {
+    console.log('found e', e);
+  }
 });
 
+console.log('listening on port', 8000);
 await app.listen({ port: 8000 });
